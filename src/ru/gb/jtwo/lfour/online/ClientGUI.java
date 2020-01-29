df9b6dd6748f2cf9e8001f32200cb59b6bb58769
palandarus <klarkken@yandex.ru>
2020-01-29 16:15:18 +0300

    GB.HomeWork# 4

---
+++ src/ru/gb/jtwo/lfour/online/ClientGUI.java
@@ -0,0 +1,208 @@
+package ru.gb.jtwo.lfour.online;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.sql.Time;
+import java.util.Date;
+
+public class ClientGUI extends JFrame implements ActionListener, Thread.UncaughtExceptionHandler, KeyListener, WindowListener {
+
+    private static final int WIDTH = 400;
+    private static final int HEIGHT = 300;
+    private OutputStream os = null;
+    private String userName, filePath = "log.txt";
+    FileWriter fr = null;
+
+    private final JTextArea log = new JTextArea();
+    private final JPanel panelTop = new JPanel(new GridLayout(2, 3));
+    private final JTextField tfIPAddress = new JTextField("127.0.0.1");
+    private final JTextField tfPort = new JTextField("8189");
+    private final JCheckBox cbAlwaysOnTop = new JCheckBox("Always on top");
+    private final JTextField tfLogin = new JTextField("ivan");
+    private final JPasswordField tfPassword = new JPasswordField("123");
+    private final JButton btnLogin = new JButton("Login");
+
+    private final JPanel panelBottom = new JPanel(new BorderLayout());
+    private final JButton btnDisconnect = new JButton("<html><b>Disconnect</b></html>");
+    private final JTextField tfMessage = new JTextField();
+    private final JButton btnSend = new JButton("Send");
+
+
+    private final JList<String> userList = new JList<>();
+
+    private ClientGUI() {
+        File file = new File(filePath);
+
+        if (!file.exists()) {
+            try {
+                file.createNewFile();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        try {
+            fr = new FileWriter(file, true);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+        userName = tfLogin.getText();
+        Thread.setDefaultUncaughtExceptionHandler(this);
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        setLocationRelativeTo(null);
+        setSize(WIDTH, HEIGHT);
+        log.setEditable(false);
+        JScrollPane scrollLog = new JScrollPane(log);
+        JScrollPane scrollUser = new JScrollPane(userList);
+        String[] users = {"user1", "user2", "user3", "user4", "user5",
+                "user_with_an_exceptionally_long_name_in_this_chat"};
+        userList.setListData(users);
+        scrollUser.setPreferredSize(new Dimension(100, 0));
+        cbAlwaysOnTop.addActionListener(this);
+        btnSend.addActionListener(this);
+        tfMessage.addKeyListener(this);
+
+        panelTop.add(tfIPAddress);
+        panelTop.add(tfPort);
+        panelTop.add(cbAlwaysOnTop);
+        panelTop.add(tfLogin);
+        panelTop.add(tfPassword);
+        panelTop.add(btnLogin);
+        panelBottom.add(btnDisconnect, BorderLayout.WEST);
+        panelBottom.add(tfMessage, BorderLayout.CENTER);
+        panelBottom.add(btnSend, BorderLayout.EAST);
+
+        add(scrollLog, BorderLayout.CENTER);
+        add(scrollUser, BorderLayout.EAST);
+        add(panelTop, BorderLayout.NORTH);
+        add(panelBottom, BorderLayout.SOUTH);
+
+        setVisible(true);
+        addWindowListener(this);
+    }
+
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() { // Event Dispatching Thread
+                new ClientGUI();
+            }
+        });
+
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        Object src = e.getSource();
+        if (src == cbAlwaysOnTop) {
+            setAlwaysOnTop(cbAlwaysOnTop.isSelected());
+        } else if (src == btnSend) {
+            addtoLog(tfMessage.getText());
+            tfMessage.setText("");
+        } else {
+            throw new RuntimeException("Unknown source: " + src);
+        }
+    }
+
+    private void addtoLog(String text) {
+
+        String str = String.format("%tF %<tT", new Date()) + " " + userName + ": " + text+"\n";
+                log.append(str);
+        try {
+            fr.write(str);
+        } catch (IOException e) {
+            e.printStackTrace();
+            try {
+                fr.close();
+            } catch (IOException er) {
+                er.printStackTrace();
+            }
+        }
+    }
+
+    @Override
+    public void uncaughtException(Thread t, Throwable e) {
+        e.printStackTrace();
+        String msg;
+        StackTraceElement[] ste = e.getStackTrace();
+        msg = "Exception in " + t.getName() + " " +
+                e.getClass().getCanonicalName() + ": " +
+                e.getMessage() + "\n\t at " + ste[0];
+
+        JOptionPane.showMessageDialog(this, msg, "Exception", JOptionPane.ERROR_MESSAGE);
+        try {
+            fr.close();
+        } catch (IOException er) {
+            er.printStackTrace();
+        }
+
+        System.exit(1);
+    }
+
+    @Override
+    public void keyTyped(KeyEvent keyEvent) {
+
+    }
+
+    @Override
+    public void keyPressed(KeyEvent keyEvent) {
+
+    }
+
+    @Override
+    public void keyReleased(KeyEvent keyEvent) {
+        if (keyEvent.getKeyCode() == KeyEvent.VK_ENTER) {
+            if (keyEvent.getSource() == tfMessage) {
+                addtoLog(tfMessage.getText());
+                tfMessage.setText("");
+            }
+        }
+    }
+
+    @Override
+    public void windowOpened(WindowEvent windowEvent) {
+
+    }
+
+    @Override
+    public void windowClosing(WindowEvent windowEvent) {
+        try {
+            fr.close();
+        } catch (IOException er) {
+            er.printStackTrace();
+        }
+    }
+
+    @Override
+    public void windowClosed(WindowEvent windowEvent) {
+
+    }
+
+    @Override
+    public void windowIconified(WindowEvent windowEvent) {
+
+    }
+
+    @Override
+    public void windowDeiconified(WindowEvent windowEvent) {
+
+    }
+
+    @Override
+    public void windowActivated(WindowEvent windowEvent) {
+
+    }
+
+    @Override
+    public void windowDeactivated(WindowEvent windowEvent) {
+
+    }
+}
